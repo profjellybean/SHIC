f9baa9ed5972a73e36a3e723c89d455286334d3a
Tamara Kügler <tamara.kuegler@tuwien.ac.at>
2021-11-24 11:09:04 +0100

    [1.5] create ItemService and ItemServiceImpl

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ItemService.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.sepm.groupphase.backend.service;
+
+import at.ac.tuwien.sepm.groupphase.backend.service.impl.ItemServiceImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface ItemService {
+
+
+}
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ItemServiceImpl.java
@@ -0,0 +1,24 @@
+package at.ac.tuwien.sepm.groupphase.backend.service.impl;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Item;
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepm.groupphase.backend.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+   
+}
