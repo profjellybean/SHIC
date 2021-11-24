f1cb5f77275f6459fd87b6516622913b1ae50497
Tamara Kügler <tamara.kuegler@tuwien.ac.at>
2021-11-24 12:30:37 +0100

    [1.5] StorageService for Item delete

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/StorageServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/StorageServiceImpl.java
@@ -1,7 +1,9 @@
 package at.ac.tuwien.sepm.groupphase.backend.service.impl;
 
+import at.ac.tuwien.sepm.groupphase.backend.entity.Item;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Storage;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepm.groupphase.backend.repository.StorageRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,20 +14,22 @@ import java.lang.invoke.MethodHandles;
 public class StorageServiceImpl implements StorageService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private final Storage storage;
+    private final StorageRepository storageRepository;
 
     @Autowired
-    public StorageServiceImpl(Storage storage) {
-        this.storage = storage;
+    public StorageServiceImpl(StorageRepository storageRepository) {
+        this.storageRepository = storageRepository;
     }
 
     @Override
-    public void deleteItemById(Long id) {
+    public Item deleteItemById(Long id) {
         LOGGER.debug("Delete item by id");
-        try{
-            storage.getItems();
+        try {
+            Item itemToDelete = storageRepository.getById(id);
+            storageRepository.delete(itemToDelete);
+            return itemToDelete;
         } catch (NotFoundException e) {
-
+            throw new NotFoundException();
         }
     }
 }
