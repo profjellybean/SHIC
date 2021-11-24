65fbae6fe97489bc0c66aba9df4fa72b049baf1b
Emil Schneider <e11909403@student.tuwien.ac.at>
2021-11-24 10:41:12 +0100

    [1.1] create item structure

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Item.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Item.java
@@ -1,4 +1,30 @@
 package at.ac.tuwien.sepm.groupphase.backend.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "Item")
 public class Item {
+    @Id
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "Notes")
+    private String notes;
+
+    @Column(name = "Image")
+    private byte[] image;
+
+
+    private Date expDate;
+
+
+
+
 }
