796f747dd677beb65b50c6720fc9e6ae5d2d7b8a
Emil Schneider <e11909403@student.tuwien.ac.at>
2021-11-26 19:25:31 +0100

    [6] Add user

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepository.java
@@ -1,25 +1,15 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
-//TODO: replace this class with a correct ApplicationUser JPARepository implementation
+
+import java.util.Optional;
+
 @Repository
-public class UserRepository {
+public interface UserRepository {
 
-    private final ApplicationUser user;
+    Optional<ApplicationUser> findUserByUsername(String username);
 
-    @Autowired
-    public UserRepository(PasswordEncoder passwordEncoder) {
-        user = new ApplicationUser("username", passwordEncoder.encode("password"));
-    }
-
-    public ApplicationUser findUserByUsername(String username) {
-        if (username.equals(user.getUsername())) {
-            return user;
-        }
-        return null; // In this case null is returned to fake Repository behavior
-    }
+    void createUser(ApplicationUser newUser);
 }
