a6c50f35d6e05fb3b802f8ceda6e4b10138ad939
Emil Schneider <e11909403@student.tuwien.ac.at>
2021-11-28 12:47:51 +0100

    [6] Add user

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,67 @@
+package at.ac.tuwien.sepm.groupphase.backend.repository.impl;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepm.groupphase.backend.exception.UsernameTakenException;
+import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+//TODO: replace this class with a correct ApplicationUser JPARepository implementation
+@Transactional
+@Repository
+
+public class UserRepositoryImpl implements UserRepository {
+
+    private ApplicationUser user;
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    public UserRepositoryImpl( EntityManager entityManager) {
+
+        this.entityManager = entityManager;
+       // user = new ApplicationUser("username","password");
+    }
+
+    @Override
+    public Optional<ApplicationUser> findUserByUsername(String username) {
+        LOGGER.info("Repository: findUserbyName {}", username);
+        if (username.equals(user.getUsername())) {
+            return Optional.of(user);
+        }
+        return Optional.empty();
+    }
+
+
+    @Override
+    public void createUser(ApplicationUser newUser) {
+
+        LOGGER.debug("Repository: UserRepositoryImpl createUser {}", newUser.getUsername());
+        TypedQuery<ApplicationUser> query = entityManager.createNamedQuery("findByName", ApplicationUser.class);
+        query.setParameter("username",newUser.getUsername());
+
+        if(query.getResultList().size() > 0) throw new UsernameTakenException("Username already taken");
+
+        entityManager.persist(newUser);
+
+    }
+
+}
