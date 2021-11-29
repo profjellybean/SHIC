6459ae78086935d64ba9ea2b107c289456e46342
Paul Spörker <paul.spoerker@tuwien.ac.at>
2021-11-29 16:56:12 +0100

    [2.3] added method planRecipe in ShoppingListEndpoint, added TODOs

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ShoppingListEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ShoppingListEndpoint.java
@@ -1,19 +1,27 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.DetailedMessageDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.MessageInquiryDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ShoppingListDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.MessageMapper;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.ShoppingListMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.ShoppingList;
 import at.ac.tuwien.sepm.groupphase.backend.service.MessageService;
 import at.ac.tuwien.sepm.groupphase.backend.service.ShoppingListService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.lang.invoke.MethodHandles;
 
 @RestController
+// TODO: change value to "api/v1/shoppinglist" ?
 @RequestMapping(value = "/shoppinglist")
 public class ShoppingListEndpoint {
 
@@ -27,5 +35,16 @@ public class ShoppingListEndpoint {
         this.shoppingListMapper = shoppingListMapper;
     }
 
+
+    //@Secured("ROLE_USER")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/{recipeId},{storageId}")
+    @Operation(summary = "Plan a recipe: creates shoppingList based on missing ingredients", security = @SecurityRequirement(name = "apiKey"))
+    // TODO: change paramteters to new Dto
+    public ShoppingListDto create(@PathVariable Long recipeId, @PathVariable Long storageId) {
+        LOGGER.info("Endpoint: POST /shoppinglist/{},{}", recipeId, storageId);
+        return shoppingListMapper.shoppingListToShoppingListDto(
+            shoppingListService.planRecipe(recipeId, storageId));
+    }
 
 }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ShoppingListServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ShoppingListServiceImpl.java
@@ -1,8 +1,6 @@
 package at.ac.tuwien.sepm.groupphase.backend.service.impl;
 
-import at.ac.tuwien.sepm.groupphase.backend.entity.Recipe;
-import at.ac.tuwien.sepm.groupphase.backend.entity.ShoppingList;
-import at.ac.tuwien.sepm.groupphase.backend.entity.Storage;
+import at.ac.tuwien.sepm.groupphase.backend.entity.*;
 import at.ac.tuwien.sepm.groupphase.backend.repository.RecipeRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.ShoppingListRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.StorageRepository;
@@ -12,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.lang.invoke.MethodHandles;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class ShoppingListServiceImpl implements ShoppingListService {
@@ -31,11 +32,51 @@ public class ShoppingListServiceImpl implements ShoppingListService {
 
     @Override
     public ShoppingList planRecipe(Long recipeId, Long storageId) {
-        LOGGER.debug("plan Recipe {} based on storage {}.", recipeId, storageId);
+        LOGGER.debug("Service: plan Recipe {} based on storage {}.", recipeId, storageId);
 
+        // TODO: check if user has access
+        // TODO: catch errors that might occur accessing the repository
         Recipe recipe = recipeRepository.getById(recipeId);
-        Storage storage = storageRepository.getById(storageId);
+        ItemStorage storage = storageRepository.getById(storageId);
 
-        return null;
+        ShoppingList shoppingList = new ShoppingList();
+        shoppingList.setName("Ingredients required for recipe: "+recipe.getName());
+        // TODO compare item sets
+        // shoppingList.setItems(compareItemSets(recipe.getIngredients(), storage.getItems()));
+
+        return shoppingListRepository.save(shoppingList);
+    }
+
+    /**
+     * Compares two sets of items.
+     * Checks which items of recipeIngredients don't appear in storedItems.
+     * If they appear, the amount of the Items is compared.
+     *
+     * @param recipeIngredients set of items e.g. representing ingredients of a recipe
+     * @param storedItems set of items e.g. representing the stored Items in a Storage
+     * @return Set of all Items that occur in recipeIngredients but not in storedItems
+     *      OR occur in both, but the amount in recipeIngredients is bigger than the amount in storedItems
+     */
+    private Set<Item> compareItemSets(Set<Item> recipeIngredients, Set<Item> storedItems) {
+        LOGGER.debug("Service: compareItemLists");
+        Set<Item> returnSet = new HashSet<>();
+        // stores items from Set into Map, because one cannot get items from a Set
+        Map<Long, Item> storedItemsMap = storedItems.stream().collect(Collectors.toMap(Item::getId, Function.identity()));
+        for (Item ingredient:
+            recipeIngredients) {
+            if(!storedItems.contains(ingredient)) {
+                // adds items that are not in the storage
+                returnSet.add(ingredient);
+            } else {
+                Item storedItem = storedItemsMap.get(ingredient.getId());
+                if(ingredient.getQuantity().equals(storedItem.getQuantity())) {
+                    // adds items if there is not enough in the storage
+                    if(ingredient.getAmount() > storedItem.getAmount()) returnSet.add(ingredient);
+                } else {
+                    // TODO recalculate unitOfQuantity
+                }
+            }
+        }
+        return returnSet;
     }
 }
