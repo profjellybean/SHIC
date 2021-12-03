35216a2c92e0d1419fce64774e4c8813cba87d10
Valentin Schnabl <e11848108@student.tuwien.ac.at>
2021-12-03 13:21:39 +0100

    [1] created item in frontend, made storageService and getAllItemsById

---
+++ frontend/src/app/services/storage.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Globals} from '../global/globals';
+import {Item} from '../dtos/item';
+import {Params} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private storageBaseUri: string = this.globals.backendUri + '/storage';
+
+  constructor(private httpClient: HttpClient, private globals: Globals) {
+  }
+
+  /**
+   * Loads all items from the backend
+   */
+  getItems(params: Params): Observable<Item[]> {
+    console.log('Get all items');
+    return this.httpClient.get<Item[]>(this.storageBaseUri, {params});
+  }
+  /**
+   * Persists message to the backend
+   *
+   * @param item to persist
+   */
+  createMessage(item: Item): Observable<Item> {
+    console.log('Create item');
+    return this.httpClient.post<Item>(this.storageBaseUri, item);
+  }
+}
