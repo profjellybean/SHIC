7eea678e5d8783865745cd9b36a1c94f4f249c8d
Paul Spörker <paul.spoerker@tuwien.ac.at>
2021-12-07 23:47:40 +0100

    [1.4] fixed typo in item service

--- frontend/src/app/services/item.service.ts
+++ frontend/src/app/services/item.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Globals} from '../global/globals';
 import {Observable} from 'rxjs';
-import {Item} from '../dtos/Item';
+import {Item} from '../dtos/item';
 
 
 @Injectable({
