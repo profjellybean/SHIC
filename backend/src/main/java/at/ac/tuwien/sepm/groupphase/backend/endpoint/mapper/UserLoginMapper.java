412c35f927f57062b2e0157b8b68571fb41103f3
Valentin Schnabl <e11848108@student.tuwien.ac.at>
2021-12-11 09:56:31 +0100

    [Config] made getCurrUser in frontend, refactored UserDto to userApplikationDto, created UserDto

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/UserLoginMapper.java
@@ -0,0 +1,40 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper;
+
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserLoggedInDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserLoginDto;
+import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+
+@Component
+public class UserMapper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserMapper(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public ApplicationUser dtoToEntity(UserLoginDto user, Long shoppingListId) {
+        LOGGER.debug("Mapper: User dtoToEntity");
+        return new ApplicationUser(user.getUsername(), passwordEncoder.encode(user.getPassword()), shoppingListId);
+    }
+
+    public UserLoginDto entityToDto(ApplicationUser user) {
+        LOGGER.debug("Mapper: User entityToDto");
+        return new UserLoginDto(user.getUsername(), user.getPassword());
+    }
+
+    public UserLoggedInDto entityToLoggedInDto(ApplicationUser user) {
+        LOGGER.debug("Mapper: User entityToLoggedInDto");
+        return new UserLoggedInDto(user.getId(), user.getUsername(), user.getPrivList());
+    }
+
+}
