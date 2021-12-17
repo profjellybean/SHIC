54c7713425619160ca667863b4d34de93e89a1b4
Tamara Kügler <tamara.kuegler@tuwien.ac.at>
2021-12-17 20:12:14 +0100

    [4.1] fixed reload bug at confirm payment

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RegisterService} from '../../services/register.service';
 import {ActivatedRoute} from '@angular/router';
 import {Bill} from '../../dtos/bill';
-import {ApplicationUser} from '../../dtos/applicationUser';
+import {User} from '../../dtos/User';
 import {BillService} from '../../services/bill.service';
 import {Register} from '../../dtos/register';
-import {UserService} from '../../services/user.service';
 // @ts-ignore
-import jwt_decode from 'wt-decode';
+import jwt_decode from 'jwt-decode';
 import {AuthService} from '../../services/auth.service';
+import {identifierModuleUrl} from '@angular/compiler';
 
 @Component({
   selector: 'app-register',
@@ -30,29 +30,16 @@ export class RegisterComponent implements OnInit {
   counter = 1;
   secondCounter = 1;
   billArray: Bill[] = [];
-  names: ApplicationUser[] = [];
-  notPaidNames: ApplicationUser[] = [];
-
   help: string;
+  billId: number;
 
-  bill: Bill = {
+  user: User = {
+    // @ts-ignore
+    username: jwt_decode(this.authService.getToken()).sub.trim(),
+    password: null,
     id: null,
-    registerId: null,
-    groceries: null,
-    notes: null,
-    names: null,
-    notPaidNames: null,
-    sum: null,
-    sumPerPerson: null,
-    date: null,
-    nameList: '',
-    notPaidNameList: ''
-  };
-
-  user: ApplicationUser = {
-    id: 3,
-    username: 'tom@email.com',
-    password: 'password'
+    currGroup: null,
+    privList: null
   };
 
   constructor(private registerService: RegisterService, private billService: BillService, public route: ActivatedRoute,
@@ -64,43 +51,19 @@ export class RegisterComponent implements OnInit {
     this.loadRegister(1);
   }
 
-  public confirmPayment(id: number) {
-    this.loadBill(id);
+  public confirmPayment(billId: number) {
 
-    this.registerService.confirmPayment(this.bill.id, this.user.username, 1).subscribe(
-      (register: Register) => {
+    this.registerService.confirmPayment(billId, this.user.username, 1).subscribe({
+      next: register => {
         this.register.id = register.id;
         this.register.bills = register.bills;
         this.register.monthlyPayments = register.monthlyPayments;
         this.register.monthlyBudget = register.monthlyBudget;
-      },
-    );
-    this.removeName(this.bill);
-    this.loadRegister(1);
-  }
-
-  private removeName(bill: Bill) {
-    for (let i = 0; i < this.notPaidNames.length; i++) {
-      if (this.notPaidNames[i].username === this.user.username) {
-        this.notPaidNames.splice(i, 1);
+        this.loadRegister(1);
+      }, error: err => {
+        this.defaultServiceErrorHandling(err);
       }
-    }
-  }
-
-  private loadBill(id: number) {
-    this.billService.getBillById(id).subscribe(
-      (bill: Bill) => {
-        this.bill.id = bill.id;
-        this.bill.groceries = bill.groceries;
-        this.bill.notes = bill.notes;
-        this.bill.names = bill.names;
-        this.bill.notPaidNames = bill.notPaidNames;
-        this.bill.sum = bill.sum;
-        this.bill.sumPerPerson = bill.sumPerPerson;
-        this.bill.date = bill.date;
-        console.log('bill ' + this.bill.nameList);
-      }
-    );
+    });
   }
 
   private loadRegister(id: number) {
@@ -117,15 +80,14 @@ export class RegisterComponent implements OnInit {
           bill.nameList = '';
           bill.notPaidNameList = '';
           for (const name of bill.names) {
-            this.names[this.secondCounter] = name;
             bill.nameList = bill.nameList + name.username + ', ';
             this.secondCounter++;
           }
           this.secondCounter = 0;
           for (const name of bill.notPaidNames) {
-            this.notPaidNames[this.secondCounter] = name;
             bill.notPaidNameList = bill.notPaidNameList + name.username + ', ';
             this.secondCounter++;
+            console.log('not paid ' + bill.notPaidNameList);
           }
           this.billArray[this.counter] = bill;
           this.counter++;
--- frontend/src/app/dtos/bill.ts
+++ frontend/src/app/dtos/bill.ts
@@ -1,13 +1,13 @@
 import {Item} from './item';
-import {ApplicationUser} from './applicationUser';
+import {User} from './User';
 
 export interface Bill {
   id: number;
   registerId: number;
   groceries: Set<Item>;
   notes: string;
-  names: Set<ApplicationUser>;
-  notPaidNames: Set<ApplicationUser>;
+  names: Set<User>;
+  notPaidNames: Set<User>;
   sum: number;
   sumPerPerson: number;
   date: Date;
