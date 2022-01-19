ec75cae62642ea8e5745897f933a2e0a22c0c8a0
Valentin Schnabl <e11848108@student.tuwien.ac.at>
2022-01-19 12:37:59 +0100

    [Config] re-fixed bug in edit user where user would not get added to roles

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
@@ -87,7 +87,7 @@ public class UserEndpoint {
         try {
             userService.editUsername(newUsernameDto.getUsername(), authentication.getName());
             List<String> roles = new LinkedList<>();
-            roles.add(newUsernameDto.getUsername());
+            roles.add("ROLE_USER");
             return "{ \"token\":\"" + jwtTokenizer.getAuthToken(newUsernameDto.getUsername(), roles) + " \"}";
         } catch (UsernameTakenException e) {
             throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
